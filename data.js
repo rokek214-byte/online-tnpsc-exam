198916f511bedba4124982af31cf93559a32ba13
rokek214-byte <rokek214@gmail.com>
2026-09-21 21:49:49 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,91 @@
+const contestants = [
+  {
+    otr: "TNPSC1001",
+    name: "Arun Kumar",
+    dob: "2000-05-15",
+    gender: "Male",
+    email: "arun@gmail.com",
+    mobile: "9876543210",
+    qualification: "MCA",
+    address: "Chennai, Tamil Nadu",
+  },
+
+  {
+    otr: "TNPSC1002",
+    name: "Priya",
+    dob: "2001-02-10",
+    gender: "Female",
+    email: "priya@gmail.com",
+    mobile: "9876543211",
+    qualification: "MCA",
+    address: "Madurai, Tamil Nadu",
+  },
+
+  {
+    otr: "TNPSC1003",
+    name: "Karthik Raj",
+    dob: "1999-08-20",
+    gender: "Male",
+    email: "karthik@gmail.com",
+    mobile: "9876543212",
+    qualification: "BCA",
+    address: "Coimbatore, Tamil Nadu",
+  },
+
+  {
+    otr: "TNPSC1004",
+    name: "Divya",
+    dob: "2000-11-12",
+    gender: "Female",
+    email: "divya@gmail.com",
+    mobile: "9876543213",
+    qualification: "MCA",
+    address: "Salem, Tamil Nadu",
+  },
+
+  {
+    otr: "TNPSC1005",
+    name: "Vignesh",
+    dob: "1998-06-25",
+    gender: "Male",
+    email: "vignesh@gmail.com",
+    mobile: "9876543214",
+    qualification: "B.Sc",
+    address: "Trichy, Tamil Nadu",
+  },
+
+  {
+    otr: "TNPSC1006",
+    name: "Keerthana",
+    dob: "2001-04-18",
+    gender: "Female",
+    email: "keerthana@gmail.com",
+    mobile: "9876543215",
+    qualification: "BCA",
+    address: "Erode, Tamil Nadu",
+  },
+
+  {
+    otr: "TNPSC1007",
+    name: "Suresh",
+    dob: "1999-09-11",
+    gender: "Male",
+    email: "suresh@gmail.com",
+    mobile: "9876543216",
+    qualification: "B.Sc",
+    address: "Tirunelveli, Tamil Nadu",
+  },
+
+  {
+    otr: "TNPSC1008",
+    name: "Anitha",
+    dob: "2000-12-05",
+    gender: "Female",
+    email: "anitha@gmail.com",
+    mobile: "9876543217",
+    qualification: "MCA",
+    address: "Vellore, Tamil Nadu",
+  },
+];
+
+export default contestants;
